4ba35ee37148e798e6998969f8b4af21a05595b0
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-08-18 18:06:43 +0200

    ver 1.0 - simply get all org\dotnet Github repositories and some of its data

---
+++ Controllers/RepositoriesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GitHubRepositoryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GitHubRepositoryAPI.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class RepositoriesController : ControllerBase {
+
+        private readonly ILogger<RepositoriesController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RepositoriesController(  ILogger<RepositoriesController> logger,
+                                        IHttpClientFactory factory) {
+            _logger = logger;
+            _httpClientFactory = factory;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Repository>> Get()
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".Net Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+            }
+
+            return repositories;
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+﻿GitHub repository API
+
+Create a simple REST service which will return details of given Github repository. Details should include:
+
+    full name of repository
+    description of repository
+    git clone url
+    number of stargazers
+    date of creation (ISO format)
+
+The API of the service should look as follows: GET /repositories/{owner}/{repository-name} { "fullName": "...", "description": "...", "cloneUrl": "...", "stars": 0, "createdAt": "..." }
